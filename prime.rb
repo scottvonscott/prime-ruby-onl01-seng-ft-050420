3769d48349b93129bc2680854ba72d12586a7556
Scott  Keplinger <skeplin17@gmail.com>
2020-05-08 20:16:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,9 @@ def prime?(integer)
   elsif integer > 2
     range = (2..(integer - 1))
     range.each do |num|
-      return false if integer % num == 0
+      integer % num == 0 ? false : true
+      # return false if integer % num == 0
+      # return true if integer % num != 0
     end
   else
    return true
