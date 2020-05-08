b976c055ed40de62440f06c17c54f7da8d8177cf
Scott  Keplinger <skeplin17@gmail.com>
2020-05-08 20:43:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,18 @@
+# def prime?(integer)
+#   if integer < 2
+#     return false
+#   elsif integer == 2
+#     return true
+#   else
+#     range = (2...(integer - 1)).to_a
+#     range.find do |num|
+#       integer % num == 0 ? true : false
+#       # return false if integer % num == 0
+#       # return true if integer % num != 0
+#     end
+#  end
+# end
+
 def prime?(integer)
   if integer < 2
     return false
@@ -5,10 +20,6 @@ def prime?(integer)
     return true
   else
     range = (2...(integer - 1)).to_a
-    range.find do |num|
-      integer % num == 0 ? true : false
-      # return false if integer % num == 0
-      # return true if integer % num != 0
-    end
+    range.any? {|num| integer % num == 0}
  end
 end
