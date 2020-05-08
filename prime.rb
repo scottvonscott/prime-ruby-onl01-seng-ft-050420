109af1dc4ee0cafb30789418b9820230502cc8d6
Scott  Keplinger <skeplin17@gmail.com>
2020-05-08 19:18:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 # Add  code here!
 def prime?(integer)
-  while integer > 1
+  if integer > 1
     (2..integer).each do |num|
       integer % num == 0
       return false
-  end
-  return true
+    end
+  else
+   true
 end
