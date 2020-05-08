78e3c156ebb0e2443f6e44029e052e968d7eb983
Scott  Keplinger <skeplin17@gmail.com>
2020-05-08 20:38:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(integer)
     return true
   else
     range = (2...(integer - 1)).to_a
-    range.collect do |num|
+    range.find do |num|
       integer % num == 0 ? true : false
       # return false if integer % num == 0
       # return true if integer % num != 0
