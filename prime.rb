3c2eeda6d8c571f5d86ef5561259a30a76747741
Scott  Keplinger <skeplin17@gmail.com>
2020-05-08 21:01:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+
+def prime?(integer)
+  if integer < 2
+    return false
+  elsif integer == 2
+    return true
+  else
+    range = (2...(integer - 1)).to_a
+    !range.any? {|num| integer % num == 0}
+ end
+end
