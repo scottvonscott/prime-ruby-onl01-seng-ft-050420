1f8cb50139eda24f14b067e79cd9985a109bbff0
Scott  Keplinger <skeplin17@gmail.com>
2020-05-08 19:32:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,8 @@
 def prime?(integer)
   if integer <= 0
     return false
-  elsif (2..(integer - 1)).each do |num|
+  elsif integer > 1
+    (2..(integer - 1)).each do |num|
       integer % num == 0
       return false
     end
